01f2ddebcc4a189d76593fcf3268cb87efa4c2f2
rashmithachamikara <rashmitha.chamickara@gmail.com>
2024-11-16 02:53:40 +0530

    Cart now shows unit prices, qty, and total prices correctly.

--- cart.php
+++ cart.php
@@ -24,13 +24,15 @@ session_start();
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand fw-bold" href="#">A1</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active"  href="./index.php">Home</a>
+                        <a class="nav-link active" href="./index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="./products.php">Products</a>
@@ -42,18 +44,17 @@ session_start();
                         <a class="nav-link" href="#">Contact</a>
                     </li>
                     <?php
-                        if(isset($_SESSION['username'])){                            
-                            echo "
+                    if (isset($_SESSION['username'])) {
+                        echo "
                             <li class='nav-item'>
                             <a class='nav-link' href='./users_area/profile.php'>My Account</a>
                         </li>";
-                        }
-                        else{
-                            echo "
+                    } else {
+                        echo "
                             <li class='nav-item'>
                             <a class='nav-link' href='./users_area/user_registration.php'>Register</a>
                         </li>";
-                        }
+                    }
                     ?>
                 </ul>
                 <form class="d-flex">
@@ -62,11 +63,19 @@ session_start();
                 </form>
                 <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="./cart.php"><svg width="28" height="28" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M11 27C11.5523 27 12 26.5523 12 26C12 25.4477 11.5523 25 11 25C10.4477 25 10 25.4477 10 26C10 26.5523 10.4477 27 11 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M25 27C25.5523 27 26 26.5523 26 26C26 25.4477 25.5523 25 25 25C24.4477 25 24 25.4477 24 26C24 26.5523 24.4477 27 25 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M3 5H7L10 22H26" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M10 16.6667H25.59C25.7056 16.6667 25.8177 16.6267 25.9072 16.5535C25.9966 16.4802 26.0579 16.3782 26.0806 16.2648L27.8806 7.26479C27.8951 7.19222 27.8934 7.11733 27.8755 7.04552C27.8575 6.97371 27.8239 6.90678 27.7769 6.84956C27.73 6.79234 27.6709 6.74625 27.604 6.71462C27.5371 6.68299 27.464 6.66661 27.39 6.66666H8" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <a class="nav-link" href="./cart.php"><svg width="28" height="28" viewBox="0 0 32 32"
+                                fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M11 27C11.5523 27 12 26.5523 12 26C12 25.4477 11.5523 25 11 25C10.4477 25 10 25.4477 10 26C10 26.5523 10.4477 27 11 27Z"
+                                    stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path
+                                    d="M25 27C25.5523 27 26 26.5523 26 26C26 25.4477 25.5523 25 25 25C24.4477 25 24 25.4477 24 26C24 26.5523 24.4477 27 25 27Z"
+                                    stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M3 5H7L10 22H26" stroke="black" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round" />
+                                <path
+                                    d="M10 16.6667H25.59C25.7056 16.6667 25.8177 16.6267 25.9072 16.5535C25.9966 16.4802 26.0579 16.3782 26.0806 16.2648L27.8806 7.26479C27.8951 7.19222 27.8934 7.11733 27.8755 7.04552C27.8575 6.97371 27.8239 6.90678 27.7769 6.84956C27.73 6.79234 27.6709 6.74625 27.604 6.71462C27.5371 6.68299 27.464 6.66661 27.39 6.66666H8"
+                                    stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                             <sup>
                                 <?php
@@ -74,7 +83,7 @@ session_start();
                                 ?>
                             </sup>
                             <span class="d-none">
-                                Total Price is: 
+                                Total Price is:
                                 <?php
                                 total_cart_price();
                                 ?>
@@ -83,30 +92,35 @@ session_start();
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" class="d-flex align-items-center gap-1" href="#">
-                            <svg width="28" height="28" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M24 27V24.3333C24 22.9188 23.5224 21.5623 22.6722 20.5621C21.8221 19.5619 20.669 19 19.4667 19H11.5333C10.331 19 9.17795 19.5619 8.32778 20.5621C7.47762 21.5623 7 22.9188 7 24.3333V27" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M16.5 14C18.9853 14 21 11.9853 21 9.5C21 7.01472 18.9853 5 16.5 5C14.0147 5 12 7.01472 12 9.5C12 11.9853 14.0147 14 16.5 14Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <svg width="28" height="28" viewBox="0 0 32 32" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M24 27V24.3333C24 22.9188 23.5224 21.5623 22.6722 20.5621C21.8221 19.5619 20.669 19 19.4667 19H11.5333C10.331 19 9.17795 19.5619 8.32778 20.5621C7.47762 21.5623 7 22.9188 7 24.3333V27"
+                                    stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path
+                                    d="M16.5 14C18.9853 14 21 11.9853 21 9.5C21 7.01472 18.9853 5 16.5 5C14.0147 5 12 7.01472 12 9.5C12 11.9853 14.0147 14 16.5 14Z"
+                                    stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                             <?php
-                                if(!isset($_SESSION['username'])){
-                                    echo "<span>
+                            if (!isset($_SESSION['username'])) {
+                                echo "<span>
                                     Welcome guest
                                 </span>";
-                            }else{
-                                    echo "<span>
-                                    Welcome ".$_SESSION['username']. "</span>";
-                                }
-                                ?>
+                            } else {
+                                echo "<span>
+                                    Welcome " . $_SESSION['username'] . "</span>";
+                            }
+                            ?>
                         </a>
                     </li>
                     <?php
-                    if(!isset($_SESSION['username'])){
+                    if (!isset($_SESSION['username'])) {
                         echo "<li class='nav-item'>
                         <a class='nav-link' href='./users_area/user_login.php'>
                             Login
                         </a>
                     </li>";
-                }else{
+                    } else {
                         echo "<li class='nav-item'>
                         <a class='nav-link' href='./users_area/logout.php'>
                             Logout
@@ -140,6 +154,7 @@ session_start();
                                     <tr class='d-flex flex-column d-md-table-row '>
                                         <th>Product Title</th>
                                         <th>Product Image</th>
+                                        <th>Unit price</th>
                                         <th>Quantity</th>
                                         <th>Total Price</th>
                                         <th>Remove</th>
@@ -161,15 +176,18 @@ session_start();
                                     $product_image_one = $row_product_price['product_image_one'];
                                     $product_values = array_sum($product_price);
                                     $total_price += $product_values * $product_quantity;
-                        ?>
+                                    ?>
                                     <!-- display data in cart  -->
                                     <tr class="d-flex flex-column d-md-table-row ">
                                         <td>
                                             <?php echo $product_title; ?>
                                         </td>
-                                        <td><img src="./admin/product_images/<?php echo $product_image_one; ?>" class="img-thumbnail" alt="<?php echo $product_title; ?>"></td>
+                                        <td><img src="./admin/product_images/<?php echo $product_image_one; ?>"
+                                                class="img-thumbnail" alt="<?php echo $product_title; ?>"></td>
+                                        <td><?php echo $price_table; ?></td>
                                         <td>
-                                            <input type="number" class="form-control w-50 mx-auto" min="1" name="qty_<?php echo $product_id; ?>">
+                                            <input type="number" class="form-control w-50 mx-auto" min="1"
+                                                value="<?= $product_quantity ?>" name="qty_<?php echo $product_id; ?>">
                                         </td>
                                         <?php
                                         // $total_price += $product_values * $product_quantity;
@@ -191,7 +209,7 @@ session_start();
                                         }
                                         ?>
                                         <td>
-                                            <?php echo $price_table; ?>
+                                            <?php echo $product_quantity * $price_table; ?>
                                         </td>
                                         <td><input type="checkbox" name="removeitem[]" value="<?php echo $product_id ?>"></td>
                                         <td>
@@ -203,9 +221,9 @@ session_start();
                                             <input type="submit" value="Remove" class="btn btn-primary" name="remove_cart">
                                         </td>
                                     </tr>
-                        <?php   }
+                                <?php }
                             }
-                        }else{
+                        } else {
                             echo "<h2 class='text-center text-danger'>Cart is empty</h2>";
                         }
                         ?>
@@ -228,13 +246,13 @@ session_start();
                         
                         <button class='btn btn-dark'><a class='text-light' href='./users_area/checkout.php'>Checkout</a></button>
                         ";
-                        }else{
+                        } else {
                             echo "<input type='button' value='Continue Shopping' class='btn btn-dark' name='continue_shopping'>";
                         }
-                        if(isset($_POST['continue_shopping'])){
+                        if (isset($_POST['continue_shopping'])) {
                             // echo "<script>window.open('index.php','_self');</script>";
                             // unset($_POST['continue_shopping']);
-                        }else if(isset($_POST['checkout'])){
+                        } else if (isset($_POST['checkout'])) {
                             // unset($_POST['checkout']);
                             // echo "<script>window.open('./users_area/checkout.php','_self');</script>";
                         }
