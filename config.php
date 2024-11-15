7dbf21b7397521d14c34ec79a337925be88f59c3
rashmithachamikara <rashmitha.chamickara@gmail.com>
2024-11-16 01:14:06 +0530

    Added config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+//Site
+define('SITE_NAME', 'E-Commerce');
+define('SITE_URL', 'http://localhost/ecommerce3');
+
+
+//Database
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'ecommerce');
+
+?>
