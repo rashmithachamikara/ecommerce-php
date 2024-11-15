36d44b5551fe68239f0c24201fbb79023000b3f8
rashmithachamikara <rashmitha.chamickara@gmail.com>
2024-11-16 01:15:21 +0530

    Modified and improved Header. Added Footer.

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+    <!-- Start Footer -->
+    <div class="upper-nav primary-bg p-2 px-3 text-center text-break">
+        <br>
+        <span>Hello &copy; 2023. All right reserved</span>
+        <br><br>
+    </div>
+    <!-- End Footer -->
+
+    <script src="./assets/js/bootstrap.bundle.js"></script>
+    
+</body>
+</html>
