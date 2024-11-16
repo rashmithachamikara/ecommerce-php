e18d52bdd8d6e5ba28f853420e7ae245c2df329d
Muhammed Amri <muhammedamri2002@gmail.com>
2024-11-16 08:49:42 +0530

    welcome button

--- cart.php
+++ cart.php
@@ -90,27 +90,16 @@ session_start();
                             </span>
                         </a>
                     </li>
+                    <?php
+                    $profile_link = isset($_SESSION['username']) ? "/ecommerce-php/users_area/profile.php" : "/ecommerce-php/users_area/user_login.php";
+                    ?>
                     <li class="nav-item">
-                        <a class="nav-link" class="d-flex align-items-center gap-1" href="#">
-                            <svg width="28" height="28" viewBox="0 0 32 32" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M24 27V24.3333C24 22.9188 23.5224 21.5623 22.6722 20.5621C21.8221 19.5619 20.669 19 19.4667 19H11.5333C10.331 19 9.17795 19.5619 8.32778 20.5621C7.47762 21.5623 7 22.9188 7 24.3333V27"
-                                    stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                <path
-                                    d="M16.5 14C18.9853 14 21 11.9853 21 9.5C21 7.01472 18.9853 5 16.5 5C14.0147 5 12 7.01472 12 9.5C12 11.9853 14.0147 14 16.5 14Z"
-                                    stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <a class="nav-link" class="d-flex align-items-center gap-1" href="<?= $profile_link ?>">
+                            <svg width="28" height="28" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M24 27V24.3333C24 22.9188 23.5224 21.5623 22.6722 20.5621C21.8221 19.5619 20.669 19 19.4667 19H11.5333C10.331 19 9.17795 19.5619 8.32778 20.5621C7.47762 21.5623 7 22.9188 7 24.3333V27" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M16.5 14C18.9853 14 21 11.9853 21 9.5C21 7.01472 18.9853 5 16.5 5C14.0147 5 12 7.01472 12 9.5C12 11.9853 14.0147 14 16.5 14Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
-                            <?php
-                            if (!isset($_SESSION['username'])) {
-                                echo "<span>
-                                    Welcome guest
-                                </span>";
-                            } else {
-                                echo "<span>
-                                    Welcome " . $_SESSION['username'] . "</span>";
-                            }
-                            ?>
+                            <span><?= isset($_SESSION['username']) ? "Welcome " . $_SESSION['username'] : "Welcome guest" ?></span>
                         </a>
                     </li>
                     <?php
@@ -175,7 +164,7 @@ session_start();
                                     $product_image_one = $row_product_price['product_image_one'];
                                     $product_values = array_sum($product_price);
                                     $total_price += $product_values * $product_quantity;
-                                    ?>
+                        ?>
                                     <!-- display data in cart  -->
                                     <tr class="d-flex flex-column d-md-table-row ">
                                         <td>
@@ -223,7 +212,7 @@ session_start();
 
                                         </td>
                                     </tr>
-                                <?php }
+                        <?php }
                             }
                         } else {
                             echo "<h2 class='text-center text-danger'>Cart is empty</h2>";
@@ -274,7 +263,6 @@ session_start();
                         if ($delete_run_result) {
                             echo "<script>window.open('cart.php','_self');</script>";
                         }
-
                     }
                 }
                 echo $remove_item = remove_cart_item();
